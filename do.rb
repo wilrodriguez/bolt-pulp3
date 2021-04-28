9397f66bfe1c46adeca0875ea803d738b3bf01a7
Chris Tessmer <chris.tessmer@onyxpoint.com>
2021-04-28 22:09:09 +0000

    Initial post-creation logic

--- do.rb
+++ do.rb
@@ -4,12 +4,14 @@ require 'pulp_rpm_client'
 
 require 'yaml'
 
+PULP_HOST='http://pulp.server:8082'
+
 # For all options, see:
 #
 #    https://www.rubydoc.info/gems/pulpcore_client/PulpcoreClient/Configuration
 #
 PulpcoreClient.configure do |config|
-  config.host = 'http://pulp.server:8082'
+  config.host = PULP_HOST
   config.username = 'admin'
   config.password = 'admin'
   # config.debugging = true
@@ -24,7 +26,7 @@ end
 #    https://www.rubydoc.info/gems/pulp_rpm_client/PulpRpmClient/Configuration
 #
 PulpRpmClient.configure do |config|
-  config.host = 'http://pulp.server:8082'
+  config.host = PULP_HOST
   config.username = 'admin'
   config.password = 'admin'
   config.debugging = ENV['DEBUG'].to_s.match?(/yes|true|1/i)
@@ -37,9 +39,9 @@ def wait_for_task_to_complete( task, opts={} )
   tasks_api = PulpcoreClient::TasksApi.new
 
   # Wait for sync task to complete
-  while tasks_api.read(task).state != 'completed' do
+  while !['completed','failed'].any?{ |state| tasks_api.read(task).state == state }
     task_info = tasks_api.read(task)
-    puts " ...Waiting for task '#{task_info.name}' to complete (status: '#{task_info.state})'"
+    puts "#{Time.now} ...Waiting for task '#{task_info.name}' to complete (status: '#{task_info.state})'"
     warn "      ( pulp_href: #{task_info.pulp_href} )"
   ###        require 'pry'; binding.pry; opts[:sleep_time] = 0
     sleep opts[:sleep_time]
@@ -63,12 +65,12 @@ def wait_for_create_task_to_complete( task, opts={} )
   end
 
   if opts[:min_expected_resources] > 0 && created_resources.empty?
-    warn "WARNING: sync task created 0 resources (task: '#{sync_async_info.task}')"
+    warn "WARNING: sync task created 0 resources (task: '#{task}')"
   end
 
   if created_resources.size > opts[:max_expected_resources]
     n = created_resources.size
-    warn "WARNING: sync task created #{n} resources (task: '#{sync_async_info.task}')"
+    warn "WARNING: sync task created #{n} resources (task: '#{task}')"
     warn "As far as we know, the task should only return 1.  So, check it out with pry!"
     require 'pry'; binding.pry
   end
@@ -111,8 +113,6 @@ def create_rpm_repo_mirror(name, remote_url)
     )
     sync_async_info = repos_api.sync( repos_data.pulp_href, rpm_repository_sync_url )
 
-
-    # Validate everything has gone as planned, then get the created resource
     created_resources = wait_for_create_task_to_complete( sync_async_info.task )
     rpm_rpm_repository_version_href = created_resources.first
 
@@ -138,71 +138,105 @@ def create_rpm_repo_mirror(name, remote_url)
     dist_created_resources = wait_for_create_task_to_complete( dist_sync_info.task )
     dist_href = dist_created_resources.first
     distribution_data = distributions_api.list({base_path: name})
+    return( distribution_data )
 
   ###  require 'pry'; binding.pry
 
-  rescue PulpcoreClient::ApiError => e
+  rescue PulpcoreClient::ApiError, PulpRpmClient::ApiError => e
     puts "Exception when calling API: #{e}"
+      warn "===> #{e}\n\n#{e.backtrace.join("\n").gsub(/^/, '    ')}\n\n==> INVESTIGATE WITH PRY"
     require 'pry'; binding.pry
   end
 end
 
 
 
-def delete_rpm_repo_mirror(name, remote_url)
-  repos_api         = PulpRpmClient::RepositoriesRpmApi.new
-  remotes_api       = PulpRpmClient::RemotesRpmApi.new
-  tasks_api         = PulpcoreClient::TasksApi.new
-  repo_versions_api = PulpRpmClient::RepositoriesRpmVersionsApi.new
-  publications_api  = PulpRpmClient::PublicationsRpmApi.new
-  distributions_api = PulpRpmClient::DistributionsRpmApi.new
-
-  opts={}
-  result = nil
+def delete_rpm_repo(name)
+  repos_api = PulpRpmClient::RepositoriesRpmApi.new
   async_responses = []
+  repos_list = repos_api.list(name: name)
+  if repos_list.count > 0
+    repos_list.results.each do |repo_data|
+      warn "!! DELETING repo #{repo_data.name}: #{repo_data.pulp_href}"
+      async_response_data = repos_api.delete(repo_data.pulp_href)
+      async_responses << async_response_data if async_response_data
+      #repo_versions_api.delete(repo_data.versions_href) unless repo_data.versions_href.empty?
+    end
+  end
+  async_responses
+end
+
+
+def delete_rpm_remote(name)
+  api = PulpRpmClient::RemotesRpmApi.new
+  async_responses = []
+  list = api.list(name: name)
+  if list.count > 0
+    list.results.each do |data|
+      warn "!! DELETING remote #{data.name}: #{data.pulp_href}"
+      async_response_data = api.delete(data.pulp_href)
+      async_responses << async_response_data if async_response_data
+    end
+  end
+
+  async_responses
+end
+
+def delete_rpm_publication(name)
+  distributions_api = PulpRpmClient::DistributionsRpmApi.new
+  publications_api  = PulpRpmClient::PublicationsRpmApi.new
+  publication_href = nil
+  async_responses = []
+
+  distributions_list = distributions_api.list(name: name)
+  return [] unless  distributions_list.count > 0
+
+  publication_href = distributions_list.results.first.publication
+  return [] unless publication_href
+
+  publications_list = publications_api.list(name: name)
+  return [] unless  publications_list.count > 0
+
+  publications_list.results.each do |publication_data|
+    warn "!! DELETING publication: (#{name}) #{publication_data.pulp_href}"
+    async_response_data = publications_api.delete(publication_href)
+    async_responses << async_response_data if async_response_data
+  end
+
+  async_responses
+end
+
+def delete_rpm_distribution(name)
+  distributions_api = PulpRpmClient::DistributionsRpmApi.new
+  async_responses = []
+
+  # delete distribution
+  distributions_list = distributions_api.list(name: name)
+
+  if distributions_list.count > 0
+    distributions_list.results.each do |distribution_data|
+      warn "!! DELETING distribution #{distribution_data.name}: #{distribution_data.pulp_href}"
+      async_response_data = distributions_api.delete(distribution_data.pulp_href)
+      async_responses << async_response_data if async_response_data
+    end
+  end
+
+  async_responses
+end
+
+
+def delete_rpm_repo_mirror(name, remote_url)
+  async_responses = []
+
   begin
+    # queue up deletion tasks
+    # NOTE errors out the first time through; is something triggering a cascading delete?
+    async_responses += delete_rpm_repo(name)
+    async_responses += delete_rpm_remote(name)
+    async_responses += delete_rpm_publication(name)
+    async_responses += delete_rpm_distribution(name)
 
-    # delete repo
-    repos_list = repos_api.list(name: name)
-    if repos_list.count > 0
-      repos_list.results.each do |repo_data|
-        warn "!! DELETING repo #{repo_data.name}: #{repo_data.pulp_href}"
-        async_response_data, response, headers = repos_api.delete_with_http_info(repo_data.pulp_href)
-        async_responses << async_response_data if async_response_data
-        #repo_versions_api.delete(repo_data.versions_href) unless repo_data.versions_href.empty?
-      end
-    end
-
-    # delete remote
-    remotes_list = remotes_api.list(name: name)
-    if remotes_list.count > 0
-      remotes_list.results.each do |remote_data|
-        warn "!! DELETING remote #{remote_data.name}: #{remote_data.pulp_href}"
-        async_response_data, response, headers = remotes_api.delete_with_http_info(remote_data.pulp_href)
-        async_responses << async_response_data if async_response_data
-      end
-    end
-
-    # delete publication
-    publications_list = publications_api.list(name: name)
-    if publications_list.count > 0
-      publications_list.results.each do |publication_data|
-        warn "!! DELETING publication: #{publication_data.pulp_href}"
-        async_response_data, response, headers = publications_api.delete_with_http_info(publication_data.pulp_href)
-        async_responses << async_response_data if async_response_data
-      end
-    end
-
-    # delete distribution
-    distributions_list = distributions_api.list(name: name)
-    if distributions_list.count > 0
-      distributions_list.results.each do |distribution_data|
-        warn "!! DELETING distribution #{distribution_data.name}: #{distribution_data.pulp_href}"
-        async_response_data, response, headers = distributions_api.delete_with_http_info(distribution_data.pulp_href)
-        async_responses << async_response_data if async_response_data
-      end
-    end
-
+    # Wait for all deletion tasks to complete
     async_responses.each do |delete_async_info|
       next unless delete_async_info
       @delete_async_info = delete_async_info
@@ -215,37 +249,91 @@ def delete_rpm_repo_mirror(name, remote_url)
   end
 end
 
-def pry_rpm_repo_mirror(name, remote_url)
+def get_rpm_repo_mirror_distro(name, url)
+  distributions_api = PulpRpmClient::DistributionsRpmApi.new
+
+  distributions_list = distributions_api.list(name: name)
+  if distributions_list.count > 0
+    return distributions_list.results.first
+  end
+  raise "Could not find distribution '#{name}'"
+end
+
+
+def create_rpm_copy_dest_repo_from(distro)
   repos_api         = PulpRpmClient::RepositoriesRpmApi.new
   remotes_api       = PulpRpmClient::RemotesRpmApi.new
   repo_versions_api = PulpRpmClient::RepositoriesRpmVersionsApi.new
   publications_api  = PulpRpmClient::PublicationsRpmApi.new
   distributions_api = PulpRpmClient::DistributionsRpmApi.new
-
-  rpm_rpm_repository = PulpRpmClient::RpmRpmRepository.new(name: name)
-  rpm_rpm_remote = PulpRpmClient::RpmRpmRemote.new(
-    name: name,
-    url: remote_url,
-    policy: 'on_demand',
-    tls_validation: false
-  )
+  async_responses = []
 
   begin
-    require 'pry'; binding.pry
+require 'pry'; binding.pry
+
+
+    # Wait for all tasks to complete
+    async_responses.each do |async_info|
+      next unless async_info
+      @async_info = async_info
+      wait_for_task_to_complete( async_info.task )
+    end
+require 'pry'; binding.pry
+
   rescue PulpcoreClient::ApiError => e
     puts "Exception when calling API: #{e}"
     require 'pry'; binding.pry
   end
 end
 
+
+#def pry_rpm_repo_mirror(name, remote_url)
+#  repos_api         = PulpRpmClient::RepositoriesRpmApi.new
+#  remotes_api       = PulpRpmClient::RemotesRpmApi.new
+#  repo_versions_api = PulpRpmClient::RepositoriesRpmVersionsApi.new
+#  publications_api  = PulpRpmClient::PublicationsRpmApi.new
+#  distributions_api = PulpRpmClient::DistributionsRpmApi.new
+#
+#  rpm_rpm_repository = PulpRpmClient::RpmRpmRepository.new(name: name)
+#  rpm_rpm_remote = PulpRpmClient::RpmRpmRemote.new(
+#    name: name,
+#    url: remote_url,
+#    policy: 'on_demand',
+#    tls_validation: false
+#  )
+#
+#  begin
+#    require 'pry'; binding.pry
+#  rescue PulpcoreClient::ApiError => e
+#    puts "Exception when calling API: #{e}"
+#    require 'pry'; binding.pry
+#  end
+#end
+
 repos_to_mirror = {
- #'iso_appstream' => 'file:///run/ISOs/unpacked/CentOS-8.3.2011-x86_64-dvd1/AppStream/',
+ # ISO
  'pulp-baseos' => 'http://mirror.centos.org/centos/8/BaseOS/x86_64/os/',
  'pulp-appstream' => 'http://mirror.centos.org/centos/8/AppStream/x86_64/os/',
+ # EPEL
  'pulp-epel' => 'https://download.fedoraproject.org/pub/epel/8/Everything/x86_64/',
  'pulp-epel-modular'  => 'https://dl.fedoraproject.org/pub/epel/8/Modular/x86_64/',
 }
 
+mirror_distros = {}
+
 #repos_to_mirror.each { |name, url| pry_rpm_repo_mirror(name, url) }
-repos_to_mirror.each { |name, url| delete_rpm_repo_mirror(name, url) }
-repos_to_mirror.each { |name, url| create_rpm_repo_mirror(name, url) }
+
+
+CREATE_NEW, USE_EXISTING = 1 , 2
+action = USE_EXISTING
+if action == CREATE_NEW
+  repos_to_mirror.each { |name, url| delete_rpm_repo_mirror(name, url) }
+  repos_to_mirror.each { |name, url| mirror_distros[name] = create_rpm_repo_mirror(name, url) }
+  mirror_distros.each{ |name, distro| create_rpm_copy_dest_repo_from(distro) }
+elsif action == USE_EXISTING
+
+  repos_to_mirror.each { |name, url| mirror_distros[name] = get_rpm_repo_mirror_distro(name, url) }
+  mirror_distros.each{ |name, distro| create_rpm_copy_dest_repo_from(distros) }
+
+end
+
