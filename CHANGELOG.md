6e192ebbf16f3eab864f7d5d17411c3d7cfb8319
Chris Tessmer <chris.tessmer@onyxpoint.com>
2021-09-23 19:40:53 -0400

    (SIMP-MAINT) Improve docs
    This patch adds a CHANGELOG, documents several plans, and expands on
    setup/requirements in README.

---
+++ CHANGELOG.md
@@ -0,0 +1,46 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
+
+## [Unreleased]
+
+### Added
+
+- `CHANGELOG.md`
+- `Rakefile` & tasks for
+- Updated RPMs
+
+### Changed
+
+- Reworked volumes
+- Cleaned up root directory
+
+### Fixed
+
+- Filter for podman matching
+- Container destruction
+
+## [0.3.0] - 2021-08-26
+
+### Added
+
+* Bolt project & scripts ready for sharing with the team
+
+## [0.2.0] - 2021-08-11
+
+### Added
+
+* `slim_modular_repodata_fix.rb` script to correct post-slimmed modulemd data
+
+## [0.1.0] - 2021-05-20
+
+### Added
+
+* Initial working direct download + Pulp uploads
+
+[0.1.0]: https://github.com/op-ct/puppetsync/releases/tag/0.1.0
+[0.2.0]: https://github.com/op-ct/puppetsync/compare/0.1.0...0.2.0
+[0.3.0]: https://github.com/op-ct/puppetsync/compare/0.2.0...0.3.0
+[Unreleased]: https://github.com/op-ct/puppetsync/compare/0.3.0...HEAD
--- plans/in_one_container/destroy.pp
+++ plans/in_one_container/destroy.pp
@@ -4,13 +4,19 @@
 #   with `--sudo-password-prompt`
 #
 # @param targets A single target to run on (the container host)
+# @param container_name Name of target Docker/podman container
+# @param container_image Target Docker/podman image
+# @param runtime Container runtime executable to use (`undef` = autodetect)
+# @param force
+#   When `true`, skips confirmation prompt before destroying things
+# @param volumes
+#   When `true`, deletes local vol mounts (may require `--sudo-password-prompt`)
 plan pulp3::in_one_container::destroy (
   TargetSpec                    $targets         = "localhost",
   String[1]                     $user            = system::env('USER'),
   Stdlib::AbsolutePath          $container_root  = system::env('PWD'),
   String[1]                     $container_name  = lookup('pulp3::in_one_container::container_name')|$k|{'pulp'},
   String[1]                     $container_image = lookup('pulp3::in_one_container::container_image')|$k|{'pulp/pulp'},
-  Stdlib::Port                  $container_port  = lookup('pulp3::in_one_container::container_port')|$k|{8080},
   Optional[Enum[podman,docker]] $runtime         = undef,
   Boolean                       $force           = false,
   Boolean                       $volumes         = false,
